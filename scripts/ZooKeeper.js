1da136d58a17c1e282fe18622b5f3c26b0c0395b
Jhun de Andres <joshuajhun@gmail.com>
2017-03-29 08:41:09 -0600

    added class and linter

---
+++ scripts/ZooKeeper.js
@@ -0,0 +1,12 @@
+export default class ZooKeeper {
+  constructor() {
+    this.fridge = {
+      bamboo: 'plant',
+      steak: 'meat',
+      corn: 'plant',
+      grass: 'plant',
+      beef: 'meat',
+      fruit: 'plant'
+    };
+  }
+}
--- tests/filter-test.js
+++
@@ -1,5 +0,0 @@
-import { expect } from 'chai';
-import filterMe from '../scripts/filter'
-
-describe('TDD with filter', () => {
-})
