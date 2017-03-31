8edb750c9998452b0d14e56719dd4dbc75ed0003
Jhun de Andres <joshuajhun@gmail.com>
2017-03-31 10:29:46 -0600

    added stubs

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+import {assert} from 'mocha'
+import people from '../starwars/people.js'
+import planets from '../starwars/planets'
+
+describe('testing starwars data', () => {
+  it('should do stuff',  () => {
+    
+  })
+})
