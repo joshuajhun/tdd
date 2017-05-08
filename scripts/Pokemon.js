9b79acb6a5357ee7d1787ad607209bb4c7f3841e
Jhun de Andres <joshuajhun@gmail.com>
2017-05-08 11:12:47 -0600

    added passing test and refactored code to a turnary statement

--- scripts/Pokemon.js
+++ scripts/Pokemon.js
@@ -1,3 +1,16 @@
+require('locus')
 export default class Pokemon {
-  
-}
+  constructor(name) {
+    this.name = name;
+    this.attacks = {};
+  };
+
+  doThisAttack(attackName) {
+    return !this.attacks[attackName] ? 'I dont know that attack'
+                                     :`${this.name} used ${attackName} it dealt ${this.attacks[attackName]} damage!`
+  };
+
+  learnThisAttack(attackName, attackValue) {
+    this.attacks[attackName] = attackValue
+  };
+};
--- tests/Pokemon-test.js
+++ tests/Pokemon-test.js
@@ -1,9 +1,34 @@
 import { expect } from 'chai';
 import Pokemon from '../scripts/Pokemon.js'
+require('locus')
 
 describe('TDD with Pokemon cuz its lit', () => {
+
+  const pikachu = new Pokemon('pikachu')
+
   it('should probably be an instance of pokemon...', () => {
-    const charizard = new Pokemon()
-    expect(charizard).to.be.an.instanceof(Pokemon)
+
+    expect(pikachu).to.be.an.instanceof(Pokemon)
   });
+
+  it('should have a name attribute assigned to it', () => {
+    expect(pikachu.name).to.equal('pikachu')
+  })
+
+  it('should have an attacks property that is an object', () => {
+    expect(pikachu.attacks).to.be.an('object')
+  })
+
+  it('should have the ability to do attacks', () => {
+    // eval(locus)
+    expect(pikachu.doThisAttack('thunder')).to.equal('I dont know that attack')
+
+    pikachu.learnThisAttack('thunder', 100)
+
+    expect(pikachu.attacks).to.deep.equal( {thunder: 100} )
+
+    expect(pikachu.doThisAttack('thunder')).to.equal('pikachu used thunder it dealt 100 damage!')
+
+  });
+
 });
